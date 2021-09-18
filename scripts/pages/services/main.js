0ec4dd3c780eb2792a8adb7ef1ecd27fa8ca9f3f
Juanchi <gallegojpe@gmail.com>
2021-09-18 14:08:25 -0500

    first

---
+++ scripts/pages/services/main.js
@@ -0,0 +1,51 @@
+window.onload = autoTurnOn;
+        
+/*--Lights on--*/
+
+function turnOn(){
+    document.getElementById('header').className = 'lights_on';            
+    document.body.style.overflow = 'scroll'; 
+}
+
+/*--Auto Lights on--*/
+function autoTurnOn(){
+    setTimeout(unlockOverflow, 3000);
+}
+
+function unlockOverflow(){
+    document.body.style.overflow = 'scroll'; 
+}
+/*--Show Trucks Data--*/
+
+
+function mostrar(a) {
+    const tarjetasActivas = document.getElementsByClassName('active');
+    
+    for (let i = 0; i < tarjetasActivas.length; i++) {
+        tarjetasActivas[i].className = 'fleet_card';        
+    }
+
+  
+
+    window.scroll({
+        top: 2100,
+        left: 0,
+        behavior: 'smooth'
+      });
+
+    document.getElementById(a).classList.toggle('active');  
+
+}
+
+function ocultarTarjetas(){
+    const tarjetasActivas = document.getElementsByClassName('active');
+    
+    for (let i = 0; i < tarjetasActivas.length; i++) {
+        tarjetasActivas[i].className = 'fleet_card';        
+    }
+    window.scroll({
+        top: 1800,
+        left: 0,
+        behavior: 'smooth'
+      });
+}
