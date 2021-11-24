ad78671f3ff95afb159193d95f83ef734ad61f70
Juanchi <gallegojpe@gmail.com>
2021-11-24 17:52:58 -0600

    loader

--- scripts/index__loader.js
+++ scripts/index__loader.js
@@ -5,19 +5,18 @@ const loader = (x) => {
 
 
     const target = document.getElementById(`${x}`);
-    const spinner = document.getElementById('loading-indicator');
 
     const hideSpinner = () => {
         target.style.transition = 'all 1s'
         target.style.opacity = '1';
-        spinner.style.display = 'none';
+        document.getElementById('loading-indicator').style.display = 'none';
     }
 
     if (!localStorage.getItem('loader')) {
 
         localStorage.setItem('loader', true);
 
-        spinner.style.display = 'flex';
+        document.getElementById('loading-indicator').style.display = 'flex';
         target.style.opacity = '0';
 
         setTimeout(() => {
