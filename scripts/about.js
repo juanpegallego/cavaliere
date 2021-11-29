efbf1aaea9f7c093d7a374a49c733e7029a83162
Juanchi <gallegojpe@gmail.com>
2021-11-29 16:27:46 -0600

    a lot

--- scripts/about.js
+++ scripts/about.js
@@ -5,9 +5,10 @@ gsap.registerPlugin(ScrollTrigger)
 
 gsap.to('.header_title', {
     scrollTrigger: 'header',
-    duration: 1,
+    duration: .8,
     opacity:1,
-    y:'50px',
+    delay:.5,
+    x:'0',
     ease:Sine.easeOut
 })
 
@@ -25,7 +26,7 @@ gsap.to('.header_text', {
 gsap.to('.content_title', {
     scrollTrigger: '.content_title',
     duration: 1,
-    delay:1,
+    delay:.5,
     x:0,
     opacity:1,
     ease:Sine.easeOut,
@@ -62,6 +63,28 @@ gsap.to('.container', {
     delay:.5,
     opacity:1,
     x:0
+})
+
+gsap.to('.title_container', {
+    scrollTrigger: '.section',
+    duration:.5,
+    delay:.5,
+    opacity:1,
+    x:0
+})
+
+gsap.to('.sustent_title', {
+    scrollTrigger: '.sustentab_content',
+    duration:.5,
+    delay:1,
+    opacity:1,
+})
+
+gsap.to('.sustent_text', {
+    scrollTrigger: '.sustentab_content',
+    duration:.5,
+    delay:1.5,
+    opacity:1,
 })
 
 
